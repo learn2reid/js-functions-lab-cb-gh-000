6a91b2b461b245e20e333429e6087166c77803c8
Lo r <lorelle.reid@gmail.com>
2019-04-01 15:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const happyHolidayTo = (holiday, name) => {
   return `Happy ${holiday}, ${name}!`;
 }
 
-const holidayCountdown = (holiday, days) => {
+holidayCountdown = (holiday, days) => {
   return `It's ${days} until ${holiday}!`;
 }
