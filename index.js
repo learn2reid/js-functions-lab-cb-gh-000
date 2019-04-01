87d000d3037a33b39e6dbf0ec6385bfe1c5de3bf
Lo r <lorelle.reid@gmail.com>
2019-04-01 15:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// write your code below!
+const happyHolidays = () => {
+  return "Happy holidays!";
+}
+
+const happyHolidaysTo = (name) => {
+  return `Happy holidays, ${name}!`;
+}
+
+const happyHolidayTo = (holiday, name) => {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+const holidayCountdown = (holiday, days) => {
+  return `It's ${days} until ${holiday}`;
+}
